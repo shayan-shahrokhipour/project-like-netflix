a7a6f1aacba94cb99adee41993d76d21d89e8bdf
oneandonlyshayanshahrokhi <shayanshahroukhipour@gmail.com>
2026-06-03 10:21:49 -0700

    add somethings

--- src/components/Beforerun.jsx
+++ src/components/Beforerun.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
 import styles from '../assets/styles/beforerun.module.css'
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
 const Beforerun = () => {
-   
+   const [value , setValue]=useState({
+    name:'',
+    family:''
+   })
+   const {name,family}=value
+
+   const getValue=(e)=>{
+     const na = e.target.name;
+     const val = e.target.value;
+     setValue(value=>({...value ,[na] : val}))
+   }
   return (
     <section className={styles.login}>
-     <input type="text" />
-     <input type="text" />
+        <h3>add your information</h3>
+     <input type="text" name='name' value={name}  onChange={getValue} placeholder='name'/>
+     <input type="text"name='family' value={family} onChange={getValue} placeholder='family'/>
       <button>Sign In</button>
 
     </section>
